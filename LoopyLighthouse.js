e2e5932e4ccc90ef69e12336283eea307b9ad4b9
JP Niyitanga <jpniyitanga@gmail.com>
2022-12-29 10:17:19 -0500

    Looping challenge

---
+++ LoopyLighthouse.js
@@ -0,0 +1,39 @@
+/*for (i = 100; i <= 110; i++) {
+  console.log(i);
+  if (i % 2 === 0) { 
+    console.log('Loopy');
+    i++;
+  }
+}*/
+
+/*let i = 100;
+while (i <= 200) {
+  if (i % 3 === 0 && i % 2 === 0) { 
+    console.log('LoopyLighthouse');
+  } else if (i % 3 === 0) { 
+    console.log('Loopy');
+  } else if (i % 2 === 0) { 
+    console.log('Lighthouse');
+  } else {
+    console.log(i);
+  }
+  i++;
+}*/
+
+var num = 99;
+while (num >= 1) {
+  if (num === 1){
+    console.log(num + " bottle of juice on the wall! " + num + " bottle of juice! Take one down, pass it around..." + (num - 1) + " bottle of juice on the wall!");
+  } else if (num === 2){
+    console.log(num + " Bottles of juice on the wall! " + num + " bottles of juice! Take one down, pass it around..." + (num - 1) + " bottle of juice on the wall!");
+  } else {
+    console.log(num + " Bottles of juice on the wall! " + num + " bottles of juice! Take one down, pass it around..." + (num - 1) + " bottles of juice on the wall!");
+  }
+num--;
+}
+
+  
+
+
+
+
